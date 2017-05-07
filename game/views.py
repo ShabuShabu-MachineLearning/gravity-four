069095529859886408eeacb642debe9bfdc1c185
ta.tanaka <takayahilton@gmail.com>
2017-05-07 16:25:18 +0900

    sessionに碁盤の状態を書き出すようにした

--- game/views.py
+++ game/views.py
@@ -3,17 +3,21 @@ from django.shortcuts import render
 
 def display(request):
 
-    print(request.POST)
+    print(request)
+    print(request.session.get('goban_list'))
 
-    goban_list = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                  [0, 0, 0, 1, 2, 0, 0, 0, 0],
-                  [0, 0, 0, 2, 1, 2, 0, 0, 0],
-                  [0, 0, 0, 1, 2, 1, 0, 0, 0]]
+    init_goban_list = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 1, 2, 0, 0, 0, 0],
+                       [0, 0, 0, 2, 1, 2, 0, 0, 0],
+                       [0, 0, 0, 1, 2, 1, 0, 0, 0]]
+
+    goban_list = request.session.get('goban_list', init_goban_list)
+    request.session['goban_list'] = goban_list
 
     game_data = {
         'goban_list': goban_list,
